8ff2203f2e7b65878d83110ef0599455ef50683d
Felix Wong <felix.wong@okta.com>
2022-03-25 14:31:11 -0700

    derp

--- src/config.ts
+++ src/config.ts
@@ -5,4 +5,4 @@ export const OAUTH_ACCESS_TOKEN: string = process.env.SLACK_OAUTH_ACCESS_TOKEN |
 export const S3_BUCKET: string = process.env.S3_BUCKET || '';
 export const SIGNING_SECRET: string = process.env.SLACK_SIGNING_SECRET || '';
 export const VERIFICATION_TOKEN: string = process.env.SLACK_VERIFICATION_TOKEN || '';
-export const DEV_MODE: string = process.env.DEV_MODE || false;
+export const DEV_MODE: boolean = process.env.DEV_MODE || false;
